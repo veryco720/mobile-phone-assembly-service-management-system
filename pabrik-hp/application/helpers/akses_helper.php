b4944df6ad9ffc698fe63cfdac3a9d2a54d08bcd
veryco720 <veryco10.ve@gmail.com>
2026-08-10 13:56:31 +0700

    update

--- pabrik-hp/application/helpers/akses_helper.php
+++ pabrik-hp/application/helpers/akses_helper.php
@@ -4,15 +4,12 @@ function cekAkses($id_modul,$aksi='can_view')
 {
     $CI =& get_instance();
 
-    $id_level =
-        $CI->session->userdata('id_level');
+    $role = $CI->session->userdata('role');
 
-    $CI->db->where('id_level',$id_level);
+    $CI->db->where('role',$role);
     $CI->db->where('id_modul',$id_modul);
 
-    $row =
-        $CI->db->get('tb_akses')
-        ->row();
+    $row = $CI->db->get('tb_akses')->row();
 
     if(!$row)
     {
@@ -22,14 +19,18 @@ function cekAkses($id_modul,$aksi='can_view')
     return $row->$aksi == 1;
 }
 
-function cekAksesJson($id_modul, $aksi)
+
+function cekAksesJson($id_modul,$aksi)
 {
-    if(!cekAkses($id_modul, $aksi))
+    if(!cekAkses($id_modul,$aksi))
     {
-        echo json_encode([
-            'result' => false,
+        echo json_encode(array(
+
+            'result'  => false,
             'message' => 'Anda tidak memiliki hak akses'
-        ]);
+
+        ));
+
         exit;
     }
 }
